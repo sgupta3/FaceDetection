f2cc018cdffb8bcd1f1e8ba9d0084384830fce96
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-28 19:16:04 -0400

    Detects number of faces for a given image.

--- FaceDetection/ViewController.m
+++ FaceDetection/ViewController.m
@@ -10,18 +10,31 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.imageView.image = [UIImage imageNamed:@"two_people.jpeg"];
+    [self markFaces:self.imageView.image];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)markFaces:(UIImage *)facePicture
+{
+    CIImage* image = [CIImage imageWithCGImage:facePicture.CGImage];
+    CIDetector* detector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:[NSDictionary dictionaryWithObject:CIDetectorAccuracyHigh forKey:CIDetectorAccuracy]];
+    
+    NSArray* features = [detector featuresInImage:image];
+    
+    NSLog(@"Faces detected: %lu ",(unsigned long)[features count]);
 }
 
 @end
